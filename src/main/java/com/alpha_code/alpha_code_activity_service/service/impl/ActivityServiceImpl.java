6c3eaa506e7e8de6e4d0e55828310bb6c60d1153
mineduck1608 <ducdm160804@gmail.com>
2025-10-03 12:11:31 +0700

    Update ActivityServiceImpl.java

--- src/main/java/com/alpha_code/alpha_code_activity_service/service/impl/ActivityServiceImpl.java
+++ src/main/java/com/alpha_code/alpha_code_activity_service/service/impl/ActivityServiceImpl.java
@@ -3,6 +3,7 @@ package com.alpha_code.alpha_code_activity_service.service.impl;
 import com.alpha_code.alpha_code_activity_service.dto.ActivityDto;
 import com.alpha_code.alpha_code_activity_service.dto.PagedResult;
 import com.alpha_code.alpha_code_activity_service.entity.Activity;
+import com.alpha_code.alpha_code_activity_service.exception.ConflictException;
 import com.alpha_code.alpha_code_activity_service.exception.ResourceNotFoundException;
 import com.alpha_code.alpha_code_activity_service.mapper.ActivityMapper;
 import com.alpha_code.alpha_code_activity_service.repository.ActivityRepository;
@@ -65,11 +66,12 @@ public class ActivityServiceImpl implements ActivityService {
     @CacheEvict(value = "activities_list", allEntries = true)
     public ActivityDto createActivity(ActivityDto dto) {
 
-        var existed = repository.findByNameIgnoreCaseAndStatusNot(dto.getName(), 0)
-                .orElseThrow(() -> new RuntimeException("Activity already exists with name: " + dto.getName()));
+        var existed = repository.findByNameIgnoreCaseAndStatusNot(dto.getName(), 0);
 
-
-
+        if(existed.isPresent()){
+            throw new ConflictException("Activity already exists with name: " + dto.getName());
+        }
+        
         var activity = ActivityMapper.toEntity(dto);
         activity.setCreatedDate(LocalDateTime.now());
 
